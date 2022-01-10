c8b5ee89f76d1d3977e51d99b6d82e129e85ac71
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-10 16:53:35 +0530

    add messageDelete event handler

---
+++ src/events/messageDelete.ts
@@ -0,0 +1,33 @@
+import { messageHandler } from "../functions";
+import { Message, PartialMessage, TextChannel } from "discord.js";
+import { channelMention, userMention } from "@discordjs/builders";
+import { client } from "..";
+export const singleMessageDelete = async (
+    message: Message<boolean> | PartialMessage
+) => {
+    if (message.author.id === client.user.id || !message.content) return;
+    const { embed, enabled, channel } = await messageHandler(message);
+    if (!enabled) {
+        return null;
+    } else if (enabled) {
+        embed
+            .setAuthor({
+                name: message.author.username,
+                iconURL: message.author.displayAvatarURL(),
+            })
+            .setDescription(
+                `**Message sent by ${userMention(
+                    message.author.id
+                )} Deleted in ${channelMention(message.channelId)}**`
+            )
+            .addField(
+                `Message`,
+                `${message.content ? message.content : "\u200b"}`
+            )
+            .setTimestamp();
+        if (!channel) {
+            return null;
+        }
+        (channel as TextChannel).send({ embeds: [embed] });
+    }
+};
