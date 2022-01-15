f6ad2257eba7a80ef4b096d0f8d312468af7486d
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-15 22:44:02 +0530

    remove timestamps from warning mongodb model

--- src/models/warn.model.ts
+++ src/models/warn.model.ts
@@ -1,18 +1,13 @@
 import { model, models, Schema } from "mongoose";
-const warnSchema = new Schema(
-  {
-    guildId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    warnings: {
-      type: Array,
-      required: true,
-    },
+const warnSchema = new Schema({
+  guildId: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  {
-    timestamps: true,
-  }
-);
-export const warnModel = models.warn || model("warn", warnSchema);
+  warnings: {
+    type: Array,
+    required: true,
+  },
+});
+export const warnModel = models.warning || model("warning", warnSchema);
